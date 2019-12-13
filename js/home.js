ece989c5bc4eb3dce3ea13e6bfc302a1880a4294
phuonglai99 <44184526+phuonglai99@users.noreply.github.com>
2019-12-13 11:12:23 +0700

    cm

---
+++ js/home.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("td > input").change(function() {
+        current_input = $(this);
+        data = [];
+        $('tr:has(td)').each(function() {
+            if ($(this).children().find('input:checked').length > 0) {
+                data.push({
+                    date: $(this).attr("data-date"),
+                    ca: $(this).attr("data-ca"),
+                    maca: $(this).attr("data-maca")
+                })
+            }
+        });
+        $.ajax({
+            url: 'schedule/checkSameSchedule',
+            data: {
+                list: data
+            },
+            type: 'get',
+            dataType: 'json',
+            success: function(response) {
+                $('tr').css('background-color', 'white');
+                $('tr > td > input').prop("disabled", false);
+                $.each(response.disable, function(index, value) {
+                    $('tr[data-maca=' + value + ']').css('background-color', 'lightsalmon');
+                    $('tr[data-maca=' + value + '] > td > input').prop('checked', false);
+                    $('tr[data-maca=' + value + '] > td > input').prop("disabled", true);
+                });
+                $('tr:has(td)').each(function() {
+                    if ($(this).children().find('input:checked').length > 0) {
+                        $(this).css('background-color', 'aqua');
+                    }
+                });
+            }
+        });
+    })
+});
---
+++ route/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    // $user=DB::table('kythi')->get()->toarray();
+    //   dd($user);
+    // return view('welcome');
+});
+Route::get('/home', function () {
+    return view('templates.home');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/inlich', function () {
+    return view('templates.inlich');
+});
+Route::get('/inlich', function () {
+    return view('templates.inlich');
+});
+Route::get('/loadata','UserController@index');
+Route::get('/pdf','UserController@generate');
+Route::get('/print','UserController@print');
+Route::get('/demo', function () {
+    return view('templates.pdf');
+});
+Route::get('/nhapdulieu','AdminController@nhapdulieu');
+Route::post('/nhapdulieu','AdminController@importexcel');
+Route::post('/nhapdulieu1','AdminController@importexcel1');
+Route::group(['prefix' => 'schedule'], function () {
+    Route::get('/', ['as' => 'schedule.show', 'uses' => 'UserController@index1']);
+    Route::get('/checkSameSchedule', ['as' => 'schedule.checkSameSchedule', 'uses' => 'UserController@checkSameSchedule']);
+});
+
+// Route::get('/inlich','UserController@inlich');
+// Route::get('/dangki/load_data', ['uses' => 'home\UserController@load_data']);
